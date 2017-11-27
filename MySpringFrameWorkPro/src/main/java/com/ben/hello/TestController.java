5336699e23c40c2d5d77f50dcbb4a4c3271e5fc4
Ben <xiewuxiao@163.com>
2017-11-27 18:04:15 +0800

    修得mvc跳转问题

--- MySpringFrameWorkPro/src/main/java/com/ben/hello/TestController.java
+++ MySpringFrameWorkPro/src/main/java/com/ben/hello/TestController.java
@@ -2,20 +2,28 @@ package com.ben.hello;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 
 /**
  * Created by hasee on 2017/11/17.
  */
 @Controller
-@RequestMapping("/test")
+@RequestMapping("/")
 public class TestController {
-    @RequestMapping(value = "/sayHi",produces = "application/json;charset=utf-8")
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "index1";
+    }
+    @RequestMapping(value = "sayHi",produces = "application/json;charset=utf-8")
     @ResponseBody()
-    public JSONObject testController_1(@RequestParam String yourName){
+    public JSONObject testController_1( String yourName){
         JSONObject result = new JSONObject();
         result.put("name",yourName);
         result.put("msg","Hello,"+yourName);
