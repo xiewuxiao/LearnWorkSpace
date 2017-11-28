ae8cb6899fd1ca03cc176bd3e8809c67bd75bff5
Ben <xiewuxiao@163.com>
2017-11-28 16:55:41 +0800

    spring mvc 初始化环境搭建

--- MySpringFrameWorkPro/src/main/java/com/ben/hello/TestController.java
+++ MySpringFrameWorkPro/src/main/java/com/ben/hello/TestController.java
@@ -1,32 +1,28 @@
 package com.ben.hello;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 
 /**
  * Created by hasee on 2017/11/17.
  */
 @Controller
-@RequestMapping("/")
+@RequestMapping(path = {"pay"})
 public class TestController {
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = {"hello.do","ben.do"})
     public String printWelcome(ModelMap model) {
         model.addAttribute("message", "Hello world!");
+        System.out.println("test");
         return "index1";
     }
-    @RequestMapping(value = "sayHi",produces = "application/json;charset=utf-8")
-    @ResponseBody()
-    public JSONObject testController_1( String yourName){
-        JSONObject result = new JSONObject();
-        result.put("name",yourName);
-        result.put("msg","Hello,"+yourName);
-        return result;
+
+    @RequestMapping(value = {"hello"})
+    public String anotherTest(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        System.out.println("请求进来啦");
+        return "index";
     }
+
 }
